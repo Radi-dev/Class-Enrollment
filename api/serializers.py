3822ff29c6a90dd9faafb1e90d5a5fef9a292774
Radi <evaradi18@gmail.com>
2022-01-08 07:16:32 +0100

    Api serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,2 +1,26 @@
 from rest_framework import serializers
 from schema.models import Applicant, Tutor, Course, Outline
+
+
+class ApplicantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Applicant
+        fields = '__all__'
+
+
+class TutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tutor
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class OutlineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Outline
+        fields = '__all__'
