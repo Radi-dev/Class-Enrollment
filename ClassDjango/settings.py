6cb297bf9e7bd8fd00a877444f40dbe76f521bda
Radi <evaradi18@gmail.com>
2022-01-08 07:11:54 +0100

    models

--- ClassDjango/settings.py
+++ ClassDjango/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from DATA import SECRET_KEY
+from .DATA import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 ###SECRET_KEY = SECRET_KEY
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- schema/models.py
+++ schema/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+# transactions
+from decimal import Decimal
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+import datetime
 
-# Create your models here.
+
+class Applicant(models.Model):
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
+    other_name = models.CharField(max_length=20, null=True, blank=True)
+    phone_number = models.IntegerField(null=True, blank=True)
+    photo = models.ImageField(upload_to='profile_pics',
+                              blank=True, null=True)
+    thumb_photo = models.ImageField(upload_to='profile_pics_thumbs',
+                                    blank=True, null=True)
+
+    def __str__(self):
+        return f'Student {self.first_name} {self.last_name}'
+
+    def save(self, *args, **kwargs):
+        old_photo = self.photo.path if self.photo else None
+        super().save(*args, **kwargs)
+        try:
+            if old_photo != self.photo.path:
+                with open(self.photo.path, 'rb') as f:
+                    self.thumb_photo = SimpleUploadedFile(
+                        self.photo.name, f.read())
+                super().save()
+                img = Image.open(self.thumb_photo.path)
+                if img.height > 200 or img.width > 200:
+                    output_size = (200, 200)
+                    img.thumbnail(output_size)
+                    img.save(self.thumb_photo.path)
+        except:
+            pass
+
+
+class Tutor(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    about = models.CharField(null=True, blank=True)
+    photo = models.ImageField(upload_to='profile_pics',
+                              blank=True, null=True)
+    thumb_photo = models.ImageField(upload_to='profile_pics_thumbs',
+                                    blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        old_photo = self.photo.path if self.photo else None
+        super().save(*args, **kwargs)
+        try:
+            if old_photo != self.photo.path:
+                with open(self.photo.path, 'rb') as f:
+                    self.thumb_photo = SimpleUploadedFile(
+                        self.photo.name, f.read())
+                super().save()
+                img = Image.open(self.thumb_photo.path)
+                if img.height > 200 or img.width > 200:
+                    output_size = (200, 200)
+                    img.thumbnail(output_size)
+                    img.save(self.thumb_photo.path)
+        except:
+            pass
+
+
+class Course(models.Model):
+    applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Outline(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(max_length=2000, null=True, blank=True)
